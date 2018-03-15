b50864d019d63e716e0831fae5318e33dbdc015c
rajesh-kandikonda <rajesh.knvssk@gmail.com>
2018-03-15 18:06:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,4 @@ echo "<br>Welcome to OpenShift developer preview";
 
 echo "<br> Testing rolling deployment with github";
 echo "<br> Changes made to show automatic deployment";
+echo "<br> Changes made to show automatic deployment";
