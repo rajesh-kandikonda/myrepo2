7c1acdc592007f4542fa77792b0de12e7d7c8620
root <root@ip-172-31-38-70.us-west-2.compute.internal>
2018-03-15 12:44:18 +0000

    Welcome

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "<br>Welcome to OpenShift developer preview";
-
+echo "<br> New";
 echo "<br> Testing rolling deployment with github";
 echo "<br> Changes made to show automatic deployment";
 echo "<br> Changes made to show automatic deployment";
